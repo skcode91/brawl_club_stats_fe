5082b8037e76caa9bb1cb2b3488590468013446d
Krzysztof Suski <krzysztof.suski@evorain.com>
2025-04-15 18:37:02 +0200

    mui init

--- brawl_fe/src/app/_items/Setup.tsx
+++ brawl_fe/src/app/_items/Setup.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import { queryClient } from "@/services/api/queryClient";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import React, { PropsWithChildren } from "react";
 import { QueryClientProvider } from "react-query";
 
+import { Roboto } from "next/font/google";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/common/theme";
+
 const Setup: React.FC<PropsWithChildren> = (props) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      {props.children}
-    </QueryClientProvider>
+    <AppRouterCacheProvider options={{ key: "css" }}>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          {props.children}
+        </QueryClientProvider>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
   );
 };
 
